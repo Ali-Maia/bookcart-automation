ce53e6f3c78b222cd4379f4b4192dd19c751ddf5
Wellington Alves <tomalves1989@gmail.com>
2025-04-21 20:56:51 -0300

    finaliza funcionadade carrinho

---
+++ cypress/e2e/cart/adicionar-itens.cy.js
@@ -0,0 +1,21 @@
+     
+  import { CartPage } from '../../support/cart/pagina-cart';
+  
+  describe('CN001: Fluxo Principal - Adição de item ao carrinho', () => {
+    const cartPage = new CartPage()
+
+    it('Deve adicionar itens e exibir no carrinho', () => {
+      
+      cartPage.acessarHome();  
+      cartPage.adicionarLivro();  
+
+      cartPage.cartIcon().click();  
+      cartPage.cartItemByTitle('Harry Potter and the Chamber of Secrets');  
+      cartPage.cartItemByTitle('Harry Potter and the Prisoner of Azkaban');
+            
+    })
+  }) 
+
+
+  
+  
---
+++ cypress/support/cart/pagina-cart.js
@@ -0,0 +1,54 @@
+import { SELECTORS } from "./elements";
+
+export class CartPage {
+
+    acessarHome() {
+        cy.log("ACESSANDO A TELA PRINCIPAL");
+        cy.visit('https://bookcart.azurewebsites.net/');
+      }
+    
+    adicionarLivro() {
+        cy.get(SELECTORS.addCartButton[0]) // Elementos do carringo estão nessa classe
+        .filter((i, el) => el.textContent.trim() === SELECTORS.addCartButton[1]) // filtra todos os span com "Add to Cart"
+        .should('have.length.at.least', 2) //garante que pelo 2 elementos estejam na tela 
+        .then(($spans) => { // quando a verificação acima for satisfeita ele executa o bloquinho abaixo
+          cy.wrap($spans[0]).closest(SELECTORS.addCartButton[2]).wait(1000).click(); //Clica no botão pai do primeiro span com texto "Add to Cart"
+          cy.wrap($spans[1]).closest(SELECTORS.addCartButton[2]).wait(1000).click();      
+        });
+      }
+
+     cartIcon() {
+        return cy.get(SELECTORS.buttonCart[0]).contains(SELECTORS.buttonCart[1]).closest(SELECTORS.buttonCart[2]);
+     }
+  
+     checkoutButton() {
+        return cy.get(SELECTORS.checkoutButton[0]).contains(SELECTORS.checkoutButton[1]).closest(SELECTORS.checkoutButton[2]);
+     }
+   
+     clearCartButton() {
+        return cy.get(SELECTORS.clearCartButton[0]).contains(SELECTORS.clearCartButton[1]).closest(SELECTORS.clearCartButton[2])
+     }
+   
+     cartTable() {  
+        return cy.get(SELECTORS.cartTable)
+     }
+   
+     cartItemByTitle(title) {
+        return cy.get(SELECTORS.cartTable).contains(SELECTORS.cartItemCell, title).should('be.visible')
+     }
+   
+     emptyCartMessage() { 
+        return cy.contains(SELECTORS.emptyCartMessage)
+    }
+   
+     deleteItemByTitle(title) {
+        return cy.get(SELECTORS.cartTable)
+         .contains(SELECTORS.cartItemCell, title)
+         .parent()
+         .within(() => {
+           cy.get(SELECTORS.deleteItemButton).click();
+         })
+    }
+}
+
+
